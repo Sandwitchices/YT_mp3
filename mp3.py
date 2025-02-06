edf3c9b0448a2553cb3f3dde6bf870453d7a406f
Sandwitchices <charlewinrecinto@gmail.com>
2025-02-07 00:23:01 +0800

    nababaliw nako

--- mp3.py
+++ mp3.py
@@ -7,6 +7,7 @@ import logging
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import time
+import json
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -38,10 +39,33 @@ def progress_hook(d):
         conversion_progress['status'] = 'finished'
         conversion_progress['percent'] = '100%'
 
+def json_to_netscape(json_file, output_file):
+    """Convert the cookies from JSON to Netscape format."""
+    with open(json_file, 'r') as f:
+        cookies = json.load(f)
+    
+    with open(output_file, 'w') as f:
+        f.write('# Netscape HTTP Cookie File\n')
+        for cookie in cookies:
+            domain = cookie['domain']
+            flag = cookie['secure'] and 'TRUE' or 'FALSE'
+            path = cookie['path']
+            expiry = cookie['expirationDate']
+            name = cookie['name']
+            value = cookie['value']
+            # Convert to the Netscape format line
+            f.write(f"{domain}\tTRUE\t{path}\t{expiry}\t{name}\t{value}\n")
+
 def get_yt_dlp_options():
     """Return yt-dlp options with authentication."""
+    cookie_file = 'youtube_cookies.json'  # Your cookies in JSON format
+    netscape_cookies_file = 'youtube_cookies.txt'  # The output Netscape formatted file
+    
+    # Convert JSON cookies to Netscape format before use
+    json_to_netscape(cookie_file, netscape_cookies_file)
+
     return {
-        'cookiefile': 'youtube_cookies.txt',  # Path to your cookies.txt file
+        'cookiefile': netscape_cookies_file,  # Path to your converted cookies.txt file
         'retries': 5,  # Retry 5 times before failing
         'retry_sleep': 10,  # Sleep for 10 seconds between retries
         'outtmpl': os.path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s'),  # Save to temp directory
